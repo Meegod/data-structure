a8bb371c99c9a6003232c44bd648f9424c62d912
godfrey <janetomeka92@yahoo.com>
2022-06-15 13:41:14 +0100

    data-structure

---
+++ arrSum.js
@@ -0,0 +1,13 @@
+// The function is expected to return an INTEGER The function accepts INTEGER_ARRAY ar as parameter.
+ 
+function simpleArraySum(arr){
+    //write your code here
+    var arrayTotal = ar.length;
+    var totalSum=0;
+
+    for(var x =0; x < arrayTotal; x++){
+        totalSum +=ar[x];
+        
+    }
+   simpleArraySum([23,34,56,67])
+}
